b122af8f580ad2e217bf7ded8c8fdb939488d2f9
febin123 <febsep96@gmail.com>
2024-04-03 04:19:31 +0530

    added features component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import Highlights from "./Components/Highlights"
 import Model from "./Components/Model"
 
 import * as Sentry from '@sentry/react'
+import Features from "./Components/Features"
 
 const App = () => {
+  
   return (
    <main className="bg-black">
     <Navbar/>
     <Hero/>
     <Highlights/>
     <Model/>
+    <Features/>
    </main>
   )
 }
